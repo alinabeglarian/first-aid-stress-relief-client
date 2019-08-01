8d5ca67e57b33621d99511477cdedf3553fe87b1
Alina <alinabeglarian@gmail.com>
2019-08-01 14:31:47 +0200

    added sound to images

--- src/components/Images/ImagesContainer.js
+++ src/components/Images/ImagesContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { loadImages } from '../../actions/images'
 import Images from './Images'
+import Sound from 'react-sound'
 
 export class ImagesContainer extends Component {
   componentDidMount() {
@@ -18,6 +19,14 @@ export class ImagesContainer extends Component {
     return (
       <div>
         <Images images={this.props.images} handleClick={this.handleClick}/>
+        <Sound
+          url="https://actions.google.com/sounds/v1/ambiences/spring_day_forest.ogg"
+          playStatus={Sound.status.PLAYING}
+          playFromPosition={300 /* in milliseconds */}
+          onLoading={this.handleSongLoading}
+          onPlaying={this.handleSongPlaying}
+          onFinishedPlaying={this.handleSongFinishedPlaying}
+      />
       </div>
     )
   }
